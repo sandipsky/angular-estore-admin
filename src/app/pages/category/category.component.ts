30dd1437cf8fca72b3a17724eb21438dd8f55bc8
sandipsky <sandipshakya75@gmail.com>
2024-06-11 15:44:05 +0545

    changes

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { GeneralListComponent } from '../general-list/general-list.component';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [GeneralListComponent],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
 export class CategoryComponent {
-
+  showPage: string = 'list';
 }
---
+++ src/app/pages/general-list/general-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './general-list.component.html',
+  styleUrl: './general-list.component.scss'
+})
+export class GeneralListComponent {
+  
+}
